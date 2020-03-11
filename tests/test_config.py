08d12a9b9ccca2594a87d13b4184ded5c8c3fdf2
ajnisbet <ajnisbet@gmail.com>
2020-03-11 10:12:00 -0700

    Use wkt projection for files without epsg
    Fixes #4
    
    * Adds better error messages for files without projections.
    * Try to use epsg first, fall back to wkt string.
    * Remove  from Pyproj call, as it will be removed in a future version.
    * Add test for files with same projection as EU-DEM.

--- tests/test_config.py
+++ tests/test_config.py
@@ -64,7 +64,7 @@ class TestLoadConfig:
     def test_complete_dataset(self, patch_config):
         conf = config.load_config()
         assert "datasets" in conf
-        assert len(conf["datasets"]) == 5
+        assert len(conf["datasets"]) == 6
 
     def test_defaults_get_overridden(self):
         path = "tests/data/configs/non-default-values.yaml"
