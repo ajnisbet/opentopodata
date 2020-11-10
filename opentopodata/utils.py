f5bd45118e12c29f0998da74297148c1b1501565
ajnisbet <ajnisbet@gmail.com>
2020-11-09 17:09:38 -0800

    Make NODATA value configurable
    Sets default from NaN to null to address issue #13

--- opentopodata/utils.py
+++ opentopodata/utils.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pyproj
 
@@ -38,3 +40,37 @@ def reproject_latlons(lats, lons, epsg=None, wkt=None):
 
 def base_floor(x, base=1):
     return base * np.floor(x / base)
+
+
+def safe_is_nan(x):
+    """Is the value NaN (not a number).
+
+    Returns True for np.nan and nan python float. Returns False for anything
+    else, including None and non-numeric types.
+
+    Called safe because it won't raise a TypeError for non-numerics.
+
+
+    Args:
+        x: Object to check for NaN.
+
+    Returns:
+        Boolean whether the object is NaN.
+    """
+    try:
+        return math.isnan(x)
+    except TypeError:
+        return False
+
+
+def fill_na(a, value):
+    """Replace NaN values in a with provided value.
+
+    Args:
+        a: Iterable, possibly containing NaN items.
+        value: WHat NaN values should be replaced with.
+
+    Returns:
+        List same length as a, with NaN values replaced.
+    """
+    return [value if safe_is_nan(x) else x for x in a]
